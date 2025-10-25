4c3a9d1cad7dfb0a6ac659b52d0742fe8c151bdc
probstjano <jano.probst@orange.fr>
2025-10-25 09:21:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'assessment-praxis-v7.6'; // 🔄 Version hochzählen bei Updates
+const CACHE_NAME = 'OMNIA App v1'; // 🔄 Version hochzählen bei Updates
 const ASSETS_TO_CACHE = [
   '30s_assissted.html',
   '6MWT.html',
